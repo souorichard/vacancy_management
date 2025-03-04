0da2cab98f7fc73ed4ec83561d0403bbe6008866
souorichard <rodrigues.lpta@gmail.com>
2025-03-03 22:18:30 -0300

    feat: configuring the Swagger

--- src/main/java/org/richard/vacancy_management/VacancyManagementApplication.java
+++ src/main/java/org/richard/vacancy_management/VacancyManagementApplication.java
@@ -3,11 +3,20 @@ package org.richard.vacancy_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+  info = @Info(
+    title = "Vacancies Management",
+    description = "API responsible for managing vacancies",
+    version = "1"
+  )
+)
 public class VacancyManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VacancyManagementApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(VacancyManagementApplication.class, args);
+  }
 }

f98ab3eedbeef767a788cfffd595540166e168f2
souorichard <rodrigues.lpta@gmail.com>
2025-03-04 14:49:10 -0300

    feat: test validating if the job doesn't exists

--- src/test/java/org/richard/vacancy_management/modules/candidate/use_cases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/org/richard/vacancy_management/modules/candidate/use_cases/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,11 @@
 package org.richard.vacancy_management.modules.candidate.use_cases;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.richard.vacancy_management.exceptions.CandidateNotFoundException;
 import org.richard.vacancy_management.exceptions.JobNotFoundException;
+import org.richard.vacancy_management.modules.candidate.CandidateEntity;
 import org.richard.vacancy_management.modules.candidate.CandidateRepository;
 import org.richard.vacancy_management.modules.company.repositories.JobRepository;
 
@@ -38,8 +44,15 @@ public class ApplyJobCandidateUseCaseTest {
   @Test
   @DisplayName("Should not be able to apply job with job not found")
   public void should_not_be_able_to_apply_job_with_job_not_found() {
+    var candidateId = UUID.randomUUID();
+
+    var candidate = new CandidateEntity();
+    candidate.setId(candidateId);
+  
+    when(candidateRepository.findById(candidateId)).thenReturn(Optional.of(candidate));
+
     try {
-      applyJobCandidateUseCase.execute(null, null);
+      applyJobCandidateUseCase.execute(candidateId, null);
     } catch (Exception exception) {
       assertThat(exception).isInstanceOf(JobNotFoundException.class);
     }

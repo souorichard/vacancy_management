e266da165a1f4483bf74be2cc4d40a67afd30be6
souorichard <rodrigues.lpta@gmail.com>
2025-03-09 19:57:35 -0300

    feat: add memory database for test

--- src/main/java/org/richard/vacancy_management/modules/company/entities/CompanyEntity.java
+++ src/main/java/org/richard/vacancy_management/modules/company/entities/CompanyEntity.java
@@ -13,10 +13,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity(name = "companies")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CompanyEntity {
   
   @Id
--- src/test/java/org/richard/vacancy_management/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/org/richard/vacancy_management/modules/company/controllers/CreateJobControllerTest.java
@@ -1,18 +1,19 @@
 package org.richard.vacancy_management.modules.company.controllers;
 
-import java.util.UUID;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.richard.vacancy_management.modules.company.dto.CreateJobDTO;
+import org.richard.vacancy_management.modules.company.entities.CompanyEntity;
+import org.richard.vacancy_management.modules.company.repositories.CompanyRepository;
 import org.richard.vacancy_management.utils.TestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,12 +23,16 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
 
   private MockMvc mvc;
 
   @Autowired
   private WebApplicationContext context;
+
+  @Autowired
+  private CompanyRepository companyRepository;
 
   @Before
   public void setup() {
@@ -39,6 +44,16 @@ public class CreateJobControllerTest {
   @Test
   @DisplayName("Should be able to create a new job")
   public void should_be_able_to_create_a_new_job() throws Exception {
+    var company = CompanyEntity.builder()
+      .name("Company Test")
+      .username("company_test")
+      .email("company@acme.com")
+      .password("admin@1234")
+      .description("Description Test")
+      .build();
+
+    company = companyRepository.saveAndFlush(company);
+
     var job = CreateJobDTO.builder()
       .description("Description Test")
       .benefits("Benefits Test")
@@ -48,7 +63,7 @@ public class CreateJobControllerTest {
     var result = mvc.perform(MockMvcRequestBuilders.post("/company/job")
       .contentType(MediaType.APPLICATION_JSON)
       .content(TestUtils.objectToJSON(job))
-      .header("Authorization", TestUtils.generateToken(UUID.randomUUID())))
+      .header("Authorization", company.getId()))
       .andExpect(MockMvcResultMatchers.status().isOk());
 
     System.out.println(result);
